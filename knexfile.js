14a999ffd50478466db36d62bfdd04795f031bda
NikSS <nik.safe@gmail.com>
2019-02-26 17:54:03 -0800

    removed . from the knexFile

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: __dirname + './db/migrations'
+      directory: __dirname + '/db/migrations'
     },
     seeds: {
       directory: __dirname +'/db/seeds'
